8a0c933dbf9d9684c010eee289f07d36e7b704d9
FrankAcosta <frank-nova98@live.com>
2020-05-25 05:20:47 -0500

    --- Auto-Git Commit ---

--- analist.py
+++ analist.py
@@ -5,5 +5,7 @@ precios = pd.DataFrame({"Azul": [8, 7.8, 8.9, 7, 9, 7.8, 9.5, 8.8, 8.2],
                         "Naranja": [10.55, 11, 11.85, 9.5, 10.5, 11.5, 11.4, 11.56, 12]
                         })
 
-# todo menos el primero
-print(precios.iloc[1:])
+
+#reingreso = precios.iloc[1:].values / precios.iloc[:-1]
+reingreso = precios.pct_change()
+print(reingreso[1:])
