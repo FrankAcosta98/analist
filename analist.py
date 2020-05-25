18cf11c12bb584702804b60e8474d39e45733b3f
FrankAcosta <frank-nova98@live.com>
2020-05-25 05:04:55 -0500

    inicial

--- analist.py
+++ analist.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-precios = pd.DataFrame({"Blue": [8, 7.8, 8.9, 7, 9, 7.8, 9.5, 8.8, 8.2]}, {
-    "Orange": [10.55, 11, 11.85, 9.5, 10.5, 11.5, 11.4, 11.56, 12]})
+precios = pd.DataFrame({"Azul": [8, 7.8, 8.9, 7, 9, 7.8, 9.5, 8.8, 8.2],
+                        "Naranja": [10.55, 11, 11.85, 9.5, 10.5, 11.5, 11.4, 11.56, 12]
+                        })
 
-print (precios)
+# todo menos el primero
+print(precios.iloc[1:])
---
+++ readme.md
@@ -0,0 +1,6 @@
+$$ R_{t,t+1} = \frac{P_{t+1}-P{t}}{P_{t}} $$
+
+o tambien
+
+$$ R_{t,{t+1}} = \frac{P_{t+1}}{P_{t}} -1 $$
+
